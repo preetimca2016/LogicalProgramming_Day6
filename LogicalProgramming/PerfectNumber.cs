98a2b97cdacf855219841a798eeb72df133acfe1
“preetimca2016” <preetimca2016@gmail.com>
2022-08-11 22:59:08 +0530

    [Preeti} Add: Progra.cs

---
+++ LogicalProgramming/PerfectNumber.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    class PerfectNumber
+    {
+        public bool Factors(int Number, int potential_Factor)  //Parameterised method
+        {
+        if(Number % potential_Factor == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public PerfectNumber()
+        {
+            int check = 0;
+            Console.WriteLine("Enter the Number");
+            int Number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= Number / 2; i++)
+            {
+                if (Factors(Number, i))
+                {
+                    check += i;
+                }
+            }
+            if(check == Number)
+            {
+                Console.WriteLine("Number is perfect Number");
+            }
+                else
+                {
+                    Console.WriteLine("Number is not perfect Number");
+                }
+            }
+        
+    }
+}
---
+++ LogicalProgramming/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace LogicalProgramming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Logical Programming");
+            Console.WriteLine("User Enter the Number");            
+            Console.WriteLine("1: Reverse Number");
+            Console.WriteLine("2: Stop watch");
+            Console.WriteLine("3: Perfect Number");
+            int Answer = Convert.ToInt32(Console.ReadLine());
+
+            switch (Answer)
+            {
+                case 1:
+                    ReverseNumber reverse = new ReverseNumber();
+                    break;
+                case 2:
+                    StopWatch stopWatch = new StopWatch();
+                    break;
+                case 3:
+                    PerfectNumber perfectNumber = new PerfectNumber();
+                    break;
+            }
+
+        }
+    }
+}
