6505146dd7d1310ab1f6249a09c0ec8ea6b4f743
“preetimca2016” <preetimca2016@gmail.com>
2022-08-15 06:46:51 +0530

    [Add: ReverseNumber]

---
+++ LogicalProgramming/ReverseNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    class ReverseNumber
+    {
+        public ReverseNumber()
+        {
+            Console.WriteLine("Enter the Number");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            int Reversed_Num = 0;
+            string Digits = Convert.ToString(Number);
+
+            for (int i = Digits.Length - 1; i >= 0; i--)
+            {
+                Reversed_Num += (Number % 10) * (int)Math.Pow(10, i);
+                Number = Number / 10;
+            }
+            Console.WriteLine("This is Reversed Number" + Reversed_Num);
+        }
+    }
+}
