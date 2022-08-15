196802588aba024745fca25f169cc5057cf5654a
“preetimca2016” <preetimca2016@gmail.com>
2022-08-15 06:50:16 +0530

    [Add: UC3-StopWatch]

---
+++ LogicalProgramming/StopWatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    class StopWatch
+    {
+        public StopWatch()
+        {
+            Console.WriteLine("Press Enter to Start the StopWatch");
+            Console.ReadLine();
+            DateTime start = DateTime.Now;
+            Console.WriteLine("Press Enter to stop a stopwatch");
+            Console.ReadLine();
+            DateTime stop = DateTime.Now;
+            Console.WriteLine("Elapsed Time = " + (stop - start));
+        }
+    }
+}
