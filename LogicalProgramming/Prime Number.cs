9a0771aa0a3ea9d895c547c4bae8af9af66ae89b
“preetimca2016” <preetimca2016@gmail.com>
2022-08-15 07:31:30 +0530

    [Add: UC5-PrimeNumber]

--- LogicalProgramming/Prime Number.cs
+++ LogicalProgramming/Prime Number.cs
@@ -1,12 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace LogicalProgramming
 {
-    internal class Prime_Number
+    public class PrimeNumber
     {
+        public void PrimeNum()
+            {
+            Console.Write("Enter a Number : ");
+            int number = int.Parse(Console.ReadLine());
+            bool IsPrime = true;
+
+            for (int i = 2; i < number / 2; i++)
+            {
+                if (number % i == 0)
+                {
+                    IsPrime = false;
+                    break;
+                }
+            }
+            if (IsPrime)
+            {
+                Console.Write("Number is Prime.");
+            }
+            else
+            {
+                Console.Write("Number is not Prime.");
+            }            
+        }
     }
 }
